cc3042a5904ab23c15731eba06cb07fbe7552169
Gyorgy Mora <gyorgy.mora@ekata.com>
2020-03-02 19:07:03 +0100

    reorg tests

--- src/test/java/co/helmethair/scalatest/TestEngineExecutionListener.java
+++ src/test/java/co/helmethair/scalatest/TestEngineExecutionListener.java
@@ -5,16 +5,15 @@ import org.junit.platform.engine.TestDescriptor;
 import org.junit.platform.engine.TestExecutionResult;
 import org.junit.platform.engine.reporting.ReportEntry;
 
+// This listener is intentionally empty as we will veryfy function calls with Mockito spy
 public class TestEngineExecutionListener implements EngineExecutionListener {
 
     @Override
     public void dynamicTestRegistered(TestDescriptor testDescriptor) {
-
     }
 
     @Override
     public void executionSkipped(TestDescriptor testDescriptor, String reason) {
-
     }
 
     @Override
@@ -27,6 +26,5 @@ public class TestEngineExecutionListener implements EngineExecutionListener {
 
     @Override
     public void reportingEntryPublished(TestDescriptor testDescriptor, ReportEntry entry) {
-
     }
 }
