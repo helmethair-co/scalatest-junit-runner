8dbbfdd01cb180cac09c8888e5a78604d243e8a1
Gyorgy Mora <gyorgy.mora@ekata.com>
2020-03-02 19:07:21 +0100

    test for erors during class init

---
+++ src/test/java/co/helmethair/scalatest/ClassInitializationErrorTest.java
@@ -0,0 +1,25 @@
+package co.helmethair.scalatest;
+
+import org.junit.jupiter.api.Test;
+import org.junit.platform.engine.EngineDiscoveryRequest;
+import org.junit.platform.engine.ExecutionRequest;
+import org.junit.platform.engine.TestDescriptor;
+
+import static org.mockito.Mockito.spy;
+
+public class ClassInitializationErrorTest implements TestHelpers {
+    @Test
+    void failedToInitializeClass() {
+        EngineDiscoveryRequest discoveryRequest = createClassDiscoveryRequest("tests.InitErrorTest");
+        TestDescriptor discoveredTests = engine.discover(discoveryRequest, engineId);
+        TestEngineExecutionListener listener = spy(new TestEngineExecutionListener());
+        ExecutionRequest executionRequest = new ExecutionRequest(discoveredTests, listener, null);
+
+        engine.execute(executionRequest);
+
+        String testId = "[engine:scalatest]/[failed:tests.InitErrorTest]";
+
+        verifyTestStartReported(testId, listener);
+        verifyTestFailReported(testId, listener);
+    }
+}
