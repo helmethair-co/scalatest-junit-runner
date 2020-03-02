6cfef317ede1c14448a058aa4cb6a4a5e23d6f49
Gyorgy Mora <gyorgy.mora@ekata.com>
2020-03-02 19:07:03 +0100

    reorg tests

--- src/test/java/co/helmethair/scalatest/RunnerTest.java
+++ src/test/java/co/helmethair/scalatest/RunnerTest.java
@@ -1,152 +1,83 @@
 package co.helmethair.scalatest;
 
 import org.junit.jupiter.api.Test;
-import org.junit.platform.engine.*;
-import org.junit.platform.engine.discovery.ClassSelector;
-import org.junit.platform.engine.discovery.DiscoverySelectors;
+import org.junit.platform.engine.EngineDiscoveryRequest;
+import org.junit.platform.engine.ExecutionRequest;
+import org.junit.platform.engine.TestDescriptor;
 
-import java.util.Collections;
-import java.util.List;
+import static org.mockito.Mockito.spy;
 
-import static org.mockito.Mockito.*;
-
-public class RunnerTest {
+public class RunnerTest implements TestHelpers {
 
     @Test
     void failingTest() {
-        UniqueId id = UniqueId.forEngine("scalatest");
-        EngineDiscoveryRequest discoveryRequest = new EngineDiscoveryRequest() {
-            @Override
-            public <T extends DiscoverySelector> List<T> getSelectorsByType(Class<T> selectorType) {
-                if (selectorType == ClassSelector.class) {
-                    return (List<T>) Collections.singletonList(DiscoverySelectors.selectClass("tests.FailingTest"));
-                }
-                return null;
-            }
-
-            @Override
-            public <T extends DiscoveryFilter<?>> List<T> getFiltersByType(Class<T> filterType) {
-                return null;
-            }
-
-            @Override
-            public ConfigurationParameters getConfigurationParameters() {
-                return null;
-            }
-        };
-        ScalatestEngine engine = new ScalatestEngine();
-        TestDescriptor discoveredTests = engine.discover(discoveryRequest, id);
+        EngineDiscoveryRequest discoveryRequest = createClassDiscoveryRequest("tests.FailingTest");
+        TestDescriptor discoveredTests = engine.discover(discoveryRequest, engineId);
         TestEngineExecutionListener listener = spy(new TestEngineExecutionListener());
         ExecutionRequest executionRequest = new ExecutionRequest(discoveredTests, listener, null);
+
         engine.execute(executionRequest);
 
-        verify(listener, atLeastOnce()).executionStarted(
-                argThat(a -> a.getUniqueId().toString().equals("[engine:scalatest]/[suite:tests.FailingTest]/[test:FailingTest 1fails]"))
-        );
+        String failingTestId = "[engine:scalatest]/[suite:tests.FailingTest]/[test:FailingTest 1fails]";
 
-        verify(listener, atLeastOnce()).executionFinished(
-                argThat(a -> a.getUniqueId().toString().equals("[engine:scalatest]/[suite:tests.FailingTest]/[test:FailingTest 1fails]")),
-                argThat(a -> a.getThrowable().isPresent()
-                        && a.getStatus() == TestExecutionResult.Status.FAILED
-                )
-        );
+        verifyTestStartReported(failingTestId, listener);
+        verifyTestFailReported(failingTestId, listener);
 
-        verify(listener, atLeastOnce()).executionFinished(
-                argThat(a -> a.getUniqueId().toString().equals("[engine:scalatest]/[suite:tests.FailingTest]/[test:FailingTest 2skips]")),
-                argThat(a -> a.getThrowable().isPresent()
-                        && a.getStatus() == TestExecutionResult.Status.ABORTED
-                )
-        );
+        String skippedTestId = "[engine:scalatest]/[suite:tests.FailingTest]/[test:FailingTest 2skips]";
+
+        verifyTestStartNotReported(skippedTestId, listener);
+        verifyTestSkipReported(skippedTestId, listener);
     }
 
     @Test
-    void passingTest() {
-        UniqueId id = UniqueId.forEngine("scalatest");
-        EngineDiscoveryRequest discoveryRequest = new EngineDiscoveryRequest() {
-            @Override
-            public <T extends DiscoverySelector> List<T> getSelectorsByType(Class<T> selectorType) {
-                if (selectorType == ClassSelector.class) {
-                    return (List<T>) Collections.singletonList(DiscoverySelectors.selectClass("tests.FunSpecTest"));
-                }
-                return null;
-            }
-
-            @Override
-            public <T extends DiscoveryFilter<?>> List<T> getFiltersByType(Class<T> filterType) {
-                return null;
-            }
-
-            @Override
-            public ConfigurationParameters getConfigurationParameters() {
-                return null;
-            }
-        };
-        ScalatestEngine engine = new ScalatestEngine();
-        TestDescriptor discoveredTests = engine.discover(discoveryRequest, id);
+    void funSpecPassingTest() {
+        EngineDiscoveryRequest discoveryRequest = createClassDiscoveryRequest("tests.FunSpecTest");
+        TestDescriptor discoveredTests = engine.discover(discoveryRequest, engineId);
         TestEngineExecutionListener listener = spy(new TestEngineExecutionListener());
         ExecutionRequest executionRequest = new ExecutionRequest(discoveredTests, listener, null);
+
         engine.execute(executionRequest);
 
-        verify(listener, atLeastOnce()).executionStarted(
-                argThat(a -> a.getUniqueId().toString().equals("[engine:scalatest]/[suite:tests.FunSpecTest]/[test:FunspecTest runs]"))
-        );
+        String failingTestId = "[engine:scalatest]/[suite:tests.FunSpecTest]/[test:FunspecTest runs]";
 
-        verify(listener, atLeastOnce()).executionFinished(
-                argThat(a -> a.getUniqueId().toString().equals("[engine:scalatest]/[suite:tests.FunSpecTest]/[test:FunspecTest runs]")),
-                argThat(a -> !a.getThrowable().isPresent()
-                        && a.getStatus() == TestExecutionResult.Status.SUCCESSFUL
-                )
-        );
+        verifyTestStartReported(failingTestId, listener);
+        verifyTestSuccessReported(failingTestId, listener);
+    }
+
+    @Test
+    void anyFunSpecPassingTest() {
+        EngineDiscoveryRequest discoveryRequest = createClassDiscoveryRequest("tests.AnyFunSpecTest");
+        TestDescriptor discoveredTests = engine.discover(discoveryRequest, engineId);
+        TestEngineExecutionListener listener = spy(new TestEngineExecutionListener());
+        ExecutionRequest executionRequest = new ExecutionRequest(discoveredTests, listener, null);
+
+        engine.execute(executionRequest);
+
+        String failingTestId = "[engine:scalatest]/[suite:tests.AnyFunSpecTest]/[test:AnyFunSpec runs]";
+
+        verifyTestStartReported(failingTestId, listener);
+        verifyTestSuccessReported(failingTestId, listener);
     }
 
     @Test
     void nestedTest() {
-        UniqueId id = UniqueId.forEngine("scalatest");
-        EngineDiscoveryRequest discoveryRequest = new EngineDiscoveryRequest() {
-            @Override
-            public <T extends DiscoverySelector> List<T> getSelectorsByType(Class<T> selectorType) {
-                if (selectorType == ClassSelector.class) {
-                    return (List<T>) Collections.singletonList(DiscoverySelectors.selectClass("tests.NestedTest"));
-                }
-                return null;
-            }
-
-            @Override
-            public <T extends DiscoveryFilter<?>> List<T> getFiltersByType(Class<T> filterType) {
-                return null;
-            }
-
-            @Override
-            public ConfigurationParameters getConfigurationParameters() {
-                return null;
-            }
-        };
-        ScalatestEngine engine = new ScalatestEngine();
-        TestDescriptor discoveredTests = engine.discover(discoveryRequest, id);
+        EngineDiscoveryRequest discoveryRequest = createClassDiscoveryRequest("tests.NestedTest");
+        TestDescriptor discoveredTests = engine.discover(discoveryRequest, engineId);
         TestEngineExecutionListener listener = spy(new TestEngineExecutionListener());
         ExecutionRequest executionRequest = new ExecutionRequest(discoveredTests, listener, null);
+
         engine.execute(executionRequest);
 
-        verify(listener, atLeastOnce()).executionStarted(
-                argThat(a -> a.getUniqueId().toString().endsWith("nested test1 runs]"))
-        );
+        String nestedSuiteTestIdSuffix = "nested test1 runs]";
+        verifyTestStartReported(nestedSuiteTestIdSuffix, listener);
+        verifyTestSuccessReported(nestedSuiteTestIdSuffix, listener);
 
-        verify(listener, atLeastOnce()).executionStarted(
-                argThat(a -> a.getUniqueId().toString().endsWith("nested test2 runs]"))
-        );
+        String nested1TestIdSuffix = "nested test1 runs]";
+        verifyTestStartReported(nested1TestIdSuffix, listener);
+        verifyTestSuccessReported(nested1TestIdSuffix, listener);
 
-        verify(listener, atLeastOnce()).executionFinished(
-                argThat(a -> a.getUniqueId().toString().endsWith("nested test1 runs]")),
-                argThat(a -> !a.getThrowable().isPresent()
-                        && a.getStatus() == TestExecutionResult.Status.SUCCESSFUL
-                )
-        );
-
-        verify(listener, atLeastOnce()).executionFinished(
-                argThat(a -> a.getUniqueId().toString().endsWith("nested test2 runs]")),
-                argThat(a -> !a.getThrowable().isPresent()
-                        && a.getStatus() == TestExecutionResult.Status.SUCCESSFUL
-                )
-        );
+        String nested2TestIdSuffix = "nested test2 runs]";
+        verifyTestStartReported(nested2TestIdSuffix, listener);
+        verifyTestSuccessReported(nested2TestIdSuffix, listener);
     }
 }
