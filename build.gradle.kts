562ac095c80895c93b30da8028b62e950428c150
Gyorgy Mora <gyorgy.mora@ekata.com>
2020-03-05 17:28:53 +0100

    oss.sonatype nexus repo deployment

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     java
     scala
     `maven-publish`
+    signing
     jacoco
     id("com.adarshr.test-logger") version "2.0.0"
     id("io.wusa.semver-git-plugin") version "2.0.2"
@@ -78,17 +79,49 @@ val stubJavaDocJar by tasks.registering(Jar::class) {
 }
 
 publishing {
+    repositories {
+        maven {
+            val releasesRepoUrl = "https://oss.sonatype.org/service/local/staging/deploy/maven2"
+            val snapshotsRepoUrl = "https://oss.sonatype.org/content/repositories/snapshots"
+            url = uri(if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl)
+            credentials {
+                username = System.getenv("NEXUS_USER")
+                password = System.getenv("NEXUS_PASSWORD")
+            }
+        }
+    }
     publications {
         register("maven", MavenPublication::class) {
             groupId = "co.helmethair"
             artifactId = "scalatest-junit-runner"
+            version = semver.info.toString()
             from(components["java"])
             artifact(sourceJar.get())
             artifact(stubJavaDocJar.get())
 
             pom {
-                name.set("Junit5 Scalatest Runner")
-                description.set("Scalatest runner for JUnit 5")
+                name.set("$groupId:$artifactId")
+                description.set("JUnit 5 Scalatest runner")
+                url.set("https://github.com/helmethair-co/scalatest-junit-runner")
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("http://www.opensource.org/licenses/mit-license.php")
+                        distribution.set("repo")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("giurim")
+                        name.set("Gyorgy Mora")
+                        email.set("gyorgy.mora@helmethair.co")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/helmethair-co/scalatest-junit-runner.git")
+                    developerConnection.set("scm:git:ssh://github.com:helmethair-co/scalatest-junit-runner.git")
+                    url.set("https://github.com/helmethair-co/scalatest-junit-runner/tree/master")
+                }
             }
         }
     }
@@ -96,3 +129,10 @@ publishing {
 
 project.extra["artifacts"] = arrayOf("maven")
 project.version = semver.info
+signing {
+    sign(publishing.publications["maven"])
+    val signingKeyId: String? by project
+    val signingKey: String? by project
+    val signingPassword: String? by project
+    useInMemoryPgpKeys(signingKeyId, signingKey, signingPassword)
+}
