6dcdbb098bc8e687afcfa8c92435f1e5de02e45f
Gyorgy Mora <gyorgy.mora@helmethair.co>
2020-03-13 02:26:28 +0100

    intellij was too eager to clean up imports

--- scala-version-matrix/src/test/scala/co/helmethair/scalatest/example/SomeCodeTest.scala
+++ scala-version-matrix/src/test/scala/co/helmethair/scalatest/example/SomeCodeTest.scala
@@ -1,5 +1,8 @@
 package co.helmethair.scalatest.example
 
+import org.scalatest.funspec.AnyFunSpec
+import org.scalatest.matchers.should.Matchers
+
 class SomeCodeTest extends AnyFunSpec with Matchers {
   describe("someFunc in SomeCode") {
     it("calculates square") {
