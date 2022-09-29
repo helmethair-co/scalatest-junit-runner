0a746d30e7592980bdd98ef5cadec19a6420edd8
Gyorgy <gyorgy.mora@helmethair.co>
2022-09-29 12:14:09 +0200

    bump versions

--- gradle-kotlin-dsl-example/build.gradle.kts
+++ gradle-kotlin-dsl-example/build.gradle.kts
@@ -9,12 +9,12 @@ repositories {
 }
 
 dependencies {
-    implementation("org.scala-lang:scala-library:2.12.8")
+    implementation("org.scala-lang:scala-library:2.12.17")
 
-    testImplementation("org.scalatest:scalatest_2.12:3.2.0-M3")
-    testRuntimeOnly("org.junit.platform:junit-platform-engine:1.6.0")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.6.0")
-    testRuntimeOnly("co.helmethair:scalatest-junit-runner:0.1.11")
+    testImplementation("org.scalatest:scalatest_2.12:3.2.13")
+    testRuntimeOnly("org.junit.platform:junit-platform-engine:1.9.1")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.9.1")
+    testRuntimeOnly("co.helmethair:scalatest-junit-runner:0.2.0")
 }
 
 tasks {
